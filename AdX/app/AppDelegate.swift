8e47eb41c1befec5507efc87d617eea0da542175
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2022-09-14 14:21:53 +0200

    Setup project configuration

---
+++ AdX/app/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  AdX
+//
+//  Created by TELOLAHY Hugues Stéphano on 14/09/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // Root view controller
+        let rootVC = ViewController()
+        let navController = UINavigationController(rootViewController: rootVC)
+        
+        // Setup window
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = navController
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
